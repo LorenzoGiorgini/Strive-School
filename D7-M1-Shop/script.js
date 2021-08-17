66fd62b3618b19bb766de7ca99b78444195159a3
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-08-17 16:52:06 +0200

    javascript dom functions

--- D7-M1-Shop/script.js
+++ D7-M1-Shop/script.js
@@ -1,7 +1,36 @@
-
 function changeH1() {
     let h1 = document.getElementById("logo-h1")
-    h1.addEventListener("click" , )
+    h1.addEventListener("click" , ()=> {
+        h1.innerText = "changed"
+    })
 }
 
 changeH1()
+
+function changeBackGround() {
+    let background = document.getElementsByTagName("body")[0]
+    background.addEventListener("dblclick" , ()=> {
+        background.style.background = "cyan"
+    })
+}
+
+changeBackGround()
+
+
+function changeFooter () {
+    const footer = document.getElementById("copyright")
+    footer.addEventListener("click" , ()=> {
+        footer.innerText = "fake text"
+    })
+}
+
+changeFooter()
+
+
+function changeAmazonLinks() {
+  let amazonLink = document.querySelector("#merch a");
+  console.log(amazonLink)
+  amazonLink.classList.add("amazonLinks")
+}
+
+changeAmazonLinks()
