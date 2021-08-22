b0bdbb474305ede6bff6ccd54f39139f3720c51b
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-08-22 17:40:14 +0200

    added new working functions

--- M1-Weekend-Homework/index.js
+++ M1-Weekend-Homework/index.js
@@ -1,3 +1,54 @@
+let students = [
+    {
+        id: 3456,
+        name: "Jones",
+        surname: "Miller",
+        age: 29,
+        email: "miller@gmail.com"
+    }, 
+    {
+        id: 4674,
+        name: "Giorgio",
+        surname: "Rossi",
+        age: 34,
+        email: "smith.2000@outlook.it"
+    },
+    {
+        id: 9567,
+        name: "Lorenzo",
+        surname: "Giorgini",
+        age: 20,
+        email: "lorenzo@outlook.com"
+    }
+]
+
+let enrolledStudents = [
+
+]
+
+function createTd() {
+    const key = ["id" , "name", "surname","age" ,"email"]
+    const table = document.getElementsByTagName("table")[0]
+    for (let i = 0; i < students.length; i++) {
+        let newTr = document.createElement("tr")
+        for (let j = 0; j < 5; j++) {
+            let newTd = document.createElement("td")
+            newTd.innerText = students[i][key[j]]
+            newTr.appendChild(newTd)
+        }
+        let plusButton = document.createElement("button")
+        plusButton.classList.add("action-button")
+        plusButton.innerText = "+"
+        plusButton.addEventListener("click" , ()=>{
+            addButton(i)
+        })
+        newTr.appendChild(plusButton)
+        table.appendChild(newTr)
+    }
+}
+
+createTd()
+
 function changeTextButton() {
     let title = document.getElementsByTagName("title")[0]
     let h1 = document.getElementById("page-title")
@@ -23,34 +74,26 @@ function emptyTextAreaAlert() {
 emptyTextAreaAlert()
 
 
-function addButton() {
-    let student_name = document.getElementsByClassName("name")
-    let student_surname = document.getElementsByClassName("surname")
-    let add_button = document.getElementsByClassName("action-button")
-    let ul = document.getElementsByTagName("ul")[0]
-    for (let i = 0; i < add_button.length; i++) {
-        add_button[i].addEventListener("click" , ()=>{
-            let remove_button = document.createElement("button")
-            remove_button.innerText = "-"
-            remove_button.classList.add("remove-item")
-            let li = document.createElement("li")
-            li.innerText = student_name[i].innerText + " " + student_surname[i].innerText
-            li.appendChild(remove_button)
-            removeButton()
-            ul.appendChild(li)
-        })
-    }
+function addButton(_index) {
+    enrolledStudents.push(students[_index])
+    let newLi = document.createElement("li")
+    newLi.innerText = students[_index].id + ' ' + students[_index].name + ' ' + students[_index].surname + ' ' + students[_index].age + ' ' + students[_index].email
+    const ul = document.getElementsByTagName("ul")[0]
+    let minusButton = document.createElement("button")
+    minusButton.classList.add("delete-button")
+    minusButton.innerText = "-"
+    newLi.appendChild(minusButton)
+    removeButton(newLi , minusButton , ul)
+    ul.appendChild(newLi)
 }
 
-addButton()
+function removeButton(_li , _button , _ul) {
+    _button.addEventListener("click" , ()=>{
+        _ul.removeChild(_li)
+    })
+}
 
-function removeButton() {
-    let removeItem = document.getElementsByClassName("remove-item")
-    let li = document.getElementsByTagName("li")
-    console.log(li)
-    for (let i = 0; i < removeItem.length; i++) {
-        removeItem[i].addEventListener("click" , ()=>{
-            li[i].remove()
-        })
-    }
+
+function studentForm() {
+    
 }
