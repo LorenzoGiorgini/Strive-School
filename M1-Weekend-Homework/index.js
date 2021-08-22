3ee6748ce0f131c645c5e0bf94fe0ba1326ff76f
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-08-22 18:55:11 +0200

    implemented all the 15 functions

--- M1-Weekend-Homework/index.js
+++ M1-Weekend-Homework/index.js
@@ -26,9 +26,17 @@ let enrolledStudents = [
 
 ]
 
+let count = 0
+
 function createTd() {
     const key = ["id" , "name", "surname","age" ,"email"]
-    const table = document.getElementsByTagName("table")[0]
+    const tmpTable = document.getElementsByTagName("table")[0]
+    let div = document.getElementById("table-content")
+    let table = document.createElement("table")
+    let placeHolders = document.getElementById("placeholders")
+    table.appendChild(placeHolders)
+    div.appendChild(table)
+    tmpTable.remove()
     for (let i = 0; i < students.length; i++) {
         let newTr = document.createElement("tr")
         for (let j = 0; j < 5; j++) {
@@ -73,9 +81,10 @@ function emptyTextAreaAlert() {
 
 emptyTextAreaAlert()
 
-
 function addButton(_index) {
+    count += 1
     enrolledStudents.push(students[_index])
+    console.log(enrolledStudents)
     let newLi = document.createElement("li")
     newLi.innerText = students[_index].id + ' ' + students[_index].name + ' ' + students[_index].surname + ' ' + students[_index].age + ' ' + students[_index].email
     const ul = document.getElementsByTagName("ul")[0]
@@ -85,15 +94,40 @@ function addButton(_index) {
     newLi.appendChild(minusButton)
     removeButton(newLi , minusButton , ul)
     ul.appendChild(newLi)
+    let counter = document.getElementById("counter")
+    counter.innerText = count
 }
 
 function removeButton(_li , _button , _ul) {
     _button.addEventListener("click" , ()=>{
         _ul.removeChild(_li)
+        let counter = document.getElementById("counter")
+        enrolledStudents.splice(count , 1)
+        count -= 1
+        counter.innerText = count
+        if (counter.innerText === "0") {
+            counter.innerText = ""
+        }
     })
 }
 
-
 function studentForm() {
-    
+    let addStudentButton = document.getElementById("completed-form")
+    let id = document.getElementById("student-id")
+    let name = document.getElementById("student-name")
+    let surname = document.getElementById("student-surname")
+    let age = document.getElementById("student-age")
+    let email = document.getElementById("student-email")
+    addStudentButton.addEventListener("click" , ()=>{
+        students.push({
+            id: id.value,
+            name: name.value,
+            surname: surname.value,
+            age: age.value,
+            email: email.value
+        })
+        createTd()
+    })
 }
+
+studentForm()
